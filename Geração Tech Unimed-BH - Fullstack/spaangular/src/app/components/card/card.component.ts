41e1315fc089b4e379aff73cc7f0ba4a178f5335
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-06-06 14:40:14 -0300

    SPA CRUD: Desinstalado bootstra

--- Geração Tech Unimed-BH - Fullstack/spaangular/src/app/components/card/card.component.ts
+++ Geração Tech Unimed-BH - Fullstack/spaangular/src/app/components/card/card.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'card-component',
   templateUrl: 'card.component.html',
+  styleUrls: ['card.component.scss'],
 })
 export class CardComponent {}
