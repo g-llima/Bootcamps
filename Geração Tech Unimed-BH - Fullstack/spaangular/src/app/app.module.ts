32661b31aaffc1535a9e1360af5432329e537d77
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-06-06 14:15:40 -0300

    SPA CRUD: Instalado bootstrap-icons

--- Geração Tech Unimed-BH - Fullstack/spaangular/src/app/app.module.ts
+++ Geração Tech Unimed-BH - Fullstack/spaangular/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
