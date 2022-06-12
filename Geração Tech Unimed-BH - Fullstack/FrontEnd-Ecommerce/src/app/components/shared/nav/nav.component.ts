bdb01710ee02d6f77cf3ba707706364654b0cb96
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-06-12 20:46:39 -0300

    FrontEnd-Ecommerce: resolvido problema de responsividade

--- Geração Tech Unimed-BH - Fullstack/FrontEnd-Ecommerce/src/app/components/shared/nav/nav.component.ts
+++ Geração Tech Unimed-BH - Fullstack/FrontEnd-Ecommerce/src/app/components/shared/nav/nav.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  getScreenWidth: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
 }
